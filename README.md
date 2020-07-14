2dadcf21325f88d2aa4b6ea082462ccb8793449f
Palmi Moller <palmimoller@me.com>
2020-07-14 14:26:46 -0600

    Accordion component expands and collapses content based on what is clicked

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ First React Hooks Project
 
 Use of React Hooks to build reusable components. Includes example of managing state in Accordion Component with class based component for reference.
 
-Accordion Component - shows only one item at a time. When one item is clicked, the details about it are expanded while the details about the previously open item is collapsed. Accordion component keeps track of this piece of state.
+Accordion Component - shows only one item at a time. When one item is clicked, the details about it are expanded while the details about the previously open item is collapsed. Accordion component keeps track of this piece of state with react hooks.
 
-App --> prop.items --> Accordion
+Styling: Semantic-ui Accordion --> https://semantic-ui.com/modules/accordion.html
+
+The keyword 'active' in the class indicates the clicked item
--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -9,25 +9,28 @@ const Accordion = ({ items }) => {
   };
 
   const renderedItems = items.map((item, index) => {
+    // if current index is the active index, include 'active' keyword
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClicked(index)}>
+        <div
+          className={`${active} title`}
+          // same as
+          // className={`title ${active}`}
+          onClick={() => onTitleClicked(index)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`${active} content`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
