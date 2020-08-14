a686d8236d8a16b55ab7c95b09d9c6293a65b25e
Palmi Moller <palmimoller@me.com>
2020-08-14 15:24:31 -0600

    Search makes api call and renders data from wikipedia api, not formatted yet

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,11 +3,21 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('Welcome');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('sample');
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
     };
 
     search();
@@ -18,6 +28,17 @@ const Search = () => {
   // setTerm(event) {
   //   this.setState({ term: event.target.value })
   // }
+
+  const renderedResults = results.map(result => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+        </div>
+        {result.snippet}
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -31,6 +52,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
