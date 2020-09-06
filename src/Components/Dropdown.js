0681a787d60f3f3a48258b057e92c15fe81da08b
Palmi Moller <palmimoller@me.com>
2020-09-06 16:07:01 -0600

    Built out jsx block with Semantic ui tags for styling, some hard coding

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -9,7 +9,19 @@ function Dropdown({ options }) {
     );
   });
 
-  return <div>{renderedOptions}</div>;
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          {/* semantic ui takes i elements with icon className and shows an icon (i for icon, not italics) */}
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dropdown;
