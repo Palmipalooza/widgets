5461ed172033483d9baa22d3bc467831ab555c7d
Palmi Moller <palmimoller@me.com>
2020-09-09 10:36:56 -0600

    Added toggle functonality to open and close list of options

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Dropdown({ options, selected, onSelectedChange }) {
+  // State keeps track of whether or not the component is open, toggle to true as oepn, false as closed
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = options.map(option => {
     // check if selected is the same as the option we are currently iterating over
     if (option.value === selected.value) {
@@ -19,14 +22,25 @@ function Dropdown({ options, selected, onSelectedChange }) {
   });
 
   return (
+    // opening and closing the dropdown: a) condtionally render renderedOptions or b) show and hide with css
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
+          {/* ^ toggle "visible active" */}
           <i className="dropdown icon"></i>
-          {/* semantic ui takes i elements with icon className and shows an icon (i for icon, not italics) */}
+          {/* ^ semantic ui takes i elements with icon className and shows an icon (i for icon, not italics) */}
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div
+            onClick={() => setOpen(!open)}
+            className={`menu ${open ? 'visible transition' : ''}`}
+          >
+            {renderedOptions}
+          </div>
+          {/* ^ toggle "visible transition" */}
         </div>
       </div>
     </div>
